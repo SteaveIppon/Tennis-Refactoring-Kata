c4157a470a2a1121bb086723d7d621fda6f37289
Steave SUN <steave.sun@agregio-edf.com>
2022-10-24 16:47:07 +0200

    Delete unsued TennisGame classes

---
+++ java/src/main/java/Deuce.java
@@ -0,0 +1,16 @@
+class Deuce implements ResultProvider {
+    private final TennisGame4 game;
+    private final ResultProvider nextResult;
+
+    public Deuce(TennisGame4 game, ResultProvider nextResult) {
+        this.game = game;
+        this.nextResult = nextResult;
+    }
+
+    @Override
+    public TennisResult getResult() {
+        if (game.isDeuce())
+            return new TennisResult("Deuce", "");
+        return this.nextResult.getResult();
+    }
+}
